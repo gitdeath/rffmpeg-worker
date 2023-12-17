d5281d613c6b2461439b4631d807c00f68def92b
Chris <59674699+gitdeath@users.noreply.github.com>
2023-12-17 11:07:01 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-
+#sleeps to ensure healtcheck timeout didn't occur
 sleep 21
+
+# Attempt to mount file systems from /etc/fstab
 mount -a
-exec "$@"
+
+# Check the exit status of the mount command
+if [ $? -eq 0 ]; then
+    # Success CMD from Dockerfile (SSHD)
+    echo "Success: File systems mounted successfully."
+    exec "$@"
+else
+# Exits the container
+    echo "Error: Failed to mount file systems. Exiting."
+    exit 1
+fi
