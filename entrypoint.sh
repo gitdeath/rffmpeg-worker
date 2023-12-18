3753bbf8724158ddc0cdcf3d2938de59715b00ce
Chris <59674699+gitdeath@users.noreply.github.com>
2023-12-17 22:31:30 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,12 @@
 #sleeps to ensure healtcheck timeout didn't occur
 sleep 21
 
+# Add transcodessh user to the group that owns renderD128
+# This is required, because unlike video the render group is different on each machine
+renderD128_gid=$(stat -c "%g" /dev/dri/renderD128)
+groupadd --gid "$renderD128_gid" render
+usermod -a -G video transcodessh
+
 # Attempt to mount file systems from /etc/fstab
 mount -a
 
