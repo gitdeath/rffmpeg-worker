016b549128e75440706d7243f02e76956e403b55
Chris <59674699+gitdeath@users.noreply.github.com>
2024-12-26 09:56:06 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,6 +31,17 @@ trap "echo 'Shutting down...'; exit 0" SIGTERM SIGINT
 # Start the sshd service and capture its PID
 /usr/sbin/sshd -D &
 sshd_pid=$!
+# Repair SSHD, if unexpectedly terminated, or exit with error stopping the container.
+if ! kill -0 $sshd_pid 2>/dev/null; then
+    echo "Warning: SSHD process terminated unexpectedly. Attempting restart..."
+    /usr/sbin/sshd -D &
+    sshd_pid=$!
+    if ! kill -0 $sshd_pid 2>/dev/null; then
+        echo "Error: Failed to restart SSHD. Exiting."
+        exit 1
+    fi
+fi
+
 
 # Run df -h in a loop every 15 seconds - this stops the container if the NFS server share is no longer available (df -h would hang.) 
 while true; do
