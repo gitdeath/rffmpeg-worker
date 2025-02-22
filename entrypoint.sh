7c4930e53c069734bd739b89eddbbe5d09b0f4f2
Chris <59674699+gitdeath@users.noreply.github.com>
2025-02-21 22:27:08 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,7 +48,7 @@ fi
     while true; do
         if ! pgrep -x "ffmpeg" > /dev/null; then
             echo "Updating jellyfin-ffmpeg7..."
-            apt update && apt install --only-upgrade jellyfin-ffmpeg7 -y
+            apt update >/dev/null 2>&1 && apt install --only-upgrade jellyfin-ffmpeg7 -y >/dev/null 2>&1
         else
             echo "ffmpeg is running. Skipping update."
         fi
